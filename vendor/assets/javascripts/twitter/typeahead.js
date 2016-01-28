a13d44db38689ba9f03fe4e8a15401206be9726c
tangopium <andreas.eiselt@gmail.com>
2016-01-28 12:41:05 +0100

    Update typeahead.js

--- vendor/assets/javascripts/twitter/typeahead.js
+++ vendor/assets/javascripts/twitter/typeahead.js
@@ -1720,8 +1720,9 @@
                     suggestions = suggestions || [];
                     if (!canceled && rendered < that.limit) {
                         that.cancel = $.noop;
-                        rendered += suggestions.length;
-                        that._append(query, suggestions.slice(0, that.limit - rendered));
+                        var suggestionsToAppend = suggestions.slice(0, that.limit - rendered);
+                        that._append(query, suggestionsToAppend);
+                        rendered += suggestionsToAppend.length;   
                         that.async && that.trigger("asyncReceived", query);
                     }
                 }
